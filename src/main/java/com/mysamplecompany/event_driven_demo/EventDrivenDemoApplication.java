3416e160826ad38ceeff7ee1af5569cf5236a05d
Adam - The Developer <adaminiature@gmail.com>
2025-10-01 10:58:41 +0700

    initial commit

---
+++ src/main/java/com/mysamplecompany/event_driven_demo/EventDrivenDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mysamplecompany.event_driven_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDrivenDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysamplecompany/event_driven_demo/EventDrivenDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysamplecompany.event_driven_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDrivenDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
